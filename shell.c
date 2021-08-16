3f7beef8807cbbbbf49e3805ae5d34a6ad77132c
mateog91 <mateog91@gmail.com>
2021-08-16 01:58:32 +0000

    UPD: created functions creatTokens and assignTokens, called them in main to make a super simple shell

--- shell.c
+++ shell.c
@@ -16,16 +16,56 @@ void deln(char *line)
 
 }
 
+void assignTokens(char *lineReaded, char **tokens, const char *delim)
+{
+	char *token = NULL;
+	int count = 0;
+
+	token = strtok(lineReaded, delim);
+	tokens[count] = token;
+	while (token != NULL)
+	{
+		count++;
+		token = strtok(NULL, delim);
+		tokens[count] = token;
+	}
+	printf("count is %i\n", count);
+	tokens[count] = NULL;
+}
+char **creat_tokens(char *buffer, const char *delim)
+{
+	char **tokens = NULL;
+	int i = 0, count_words = 1;
+
+	while (buffer[i] != '\0')
+	{
+		if (buffer[i] == *delim)
+			count_words++;
+		i++;
+	}
+	tokens = malloc(sizeof(buffer) * (count_words + 1));
+	if (tokens == NULL)
+	{
+		perror("Error: tokens not created");
+		return (NULL);
+	}
+	
+	return (tokens);
+}
+
+
+
 
 int main(int argc, char **argv, char **env)
 {
-
 	char *line = NULL;
 	size_t len_line;
 	char *prompt = "$ ";
 	char *arg[1];
-	char *argv2[] = {"/bin/ls", "-l", "/usr/", NULL};
-
+	pid_t child_pid;
+	int status;
+	char **tokens = NULL;
+	const char *delim = " ";
 
 	UNUSED(argc);
 	UNUSED(argv);
@@ -33,22 +73,31 @@ int main(int argc, char **argv, char **env)
 	UNUSED(line);
 	UNUSED(len_line);
 	UNUSED(arg);
-	UNUSED(argv2);
 
 	while (1)
 	{
 		write(1, prompt, strlen(prompt));
 		getline(&line, &len_line, stdin);
 		deln(line);
+		tokens = creat_tokens(line, delim);
+		
+		assignTokens(line, tokens, delim);
 		arg[0] = line;
-
-/*		arg[1] = "-l";
-		arg[2] = "/usr/";*/
-		arg[3] = NULL;
-
-		if(execve(arg[0], arg, NULL) == -1)
+		arg[1] = NULL;
+		
+		child_pid = fork();
+		if (child_pid == -1)
 		{
-			perror("......Error:");
+			perror("Error:");
+			return (1);
+		}
+		wait(&status);
+		if (child_pid == 0)
+		{
+			if(execve(tokens[0], tokens, NULL) == -1)
+			{
+				perror("......Error:");
+			}
 		}
 	}
 
