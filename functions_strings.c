41b23be32322c22aec515079348ecf3baee060f7
JuanSebastianGB <3393@holbertonschool.com>
2021-08-16 13:13:06 +0000

    UPL: Uploading file function_strings to handle some used string functions

---
+++ functions_strings.c
@@ -0,0 +1,36 @@
+#include "shell.h"
+
+/**
+ * *_strcmp - compare two strings
+ *@s1: pointer 1
+ *@s2: pointer 2
+ *Return: An integer != 0 if not equal
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (*(s1 + i) && *(s2 + i))
+	{
+		if (s1[i] != s2[i])
+			break;
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
+
+/**
+ * _strlen - calculate the lenght of a input string
+ * @str: pointer string to compute lenght
+ * Return: lenght of the string
+ */
+
+int _strlen(char *str)
+{
+	int i = 0;
+	while (str[i])
+		i++;
+	
+	return (i);
+}
