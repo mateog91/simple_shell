3fca98f4a4c7930e73f8a5bdca5fe8de599d29d0
mateog91 <mateog91@gmail.com>
2021-08-16 21:02:48 +0000

    UPD: added comments for clean code

--- handling_path.c
+++ handling_path.c
@@ -1,25 +1,32 @@
 #include "shell.h"
 /**
  * *getPath - get buffer contained in route PATH
- * @env: enviroment variable
- * Return: Path
+ * @env: Matrix containing enviroment variables
  *
+ * Return: Pointer to string containing PATH=
  */
 char *getPath(char **env)
 {
         char *first_part;
         int i = 0, j;
+
+	/*i advances rows of env matrix */
+	/*j advances chars of current string in current row i */
+
         first_part = "PATH";
+
+	/*Go through vector with env viariablis in search of PATH */
         while (env[i])
         {
+		/* Compares char by char of current with PATH*/
                 for (j = 0; j < 4; j++)
                 {
-                        if (first_part[j] != env[i][j])/*Any prev match */
+                        if (first_part[j] != env[i][j]) /* Break when not = */
                         {
                                 break;
                         }
                 }
-                if (j == 4)/*All char matches */
+                if (j == 4) /* All characters match so end and return */
                         break;
                 i++;
         }
