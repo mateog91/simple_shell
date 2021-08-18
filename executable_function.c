1fe691b9a2777eaeca1bee82da15b8630df613f7
mateog91 <mateog91@gmail.com>
2021-08-18 21:47:14 +0000

    ADD: added executable_function.c to move the execution part into a seperate function outside main

---
+++ executable_function.c
@@ -0,0 +1,25 @@
+int executable_function(char *command, char **tokens)
+{
+	pid_t child_pid;
+	int status;
+
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		perror("Error:");
+		return (1);
+	}
+	wait(&status)
+	/*free(line);*/
+	if (child_pid == 0)
+	{
+	
+		/*si algo es tokens[0]*/
+		if (execve(command, tokens, NULL) == -1)
+		{
+			perror("Error: execve failed");
+			return (-1);
+		}
+	}
+	return (0);
+}
