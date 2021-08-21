4e2d2f12fd5ff366a08fe352eb5d61e278dd9712
JuanSebastianGB <3393@holbertonschool.com>
2021-08-21 22:01:55 +0000

    UPD: Adding function to print numbers

--- functions_strings_basic.c
+++ functions_strings_basic.c
@@ -1,34 +1,6 @@
 #include "shell.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-
-/**
- * _puts - prints a string followed by a new line
- * @str: - pointer to first character of string
- *
- */
-void _puts(char *str)
-{
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-		_putchar(str[i]);
-	_putchar('\n');
-}
-
-
-/**
  * *_strcmp - compare two strings
  *@s1: pointer 1
  *@s2: pointer 2
