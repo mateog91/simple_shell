356042fa11a1209ab41d3d71ffb1b3833d2b3b73
mateog91 <mateog91@gmail.com>
2021-08-22 00:18:10 +0000

    UPD: added return_error variable to function and prototype

--- functions_print_errors.c
+++ functions_print_errors.c
@@ -4,7 +4,12 @@
  *
  */
 
-void print_error_not_found(char *argv0, char *tokens0, int countExec)
+void print_error_not_found(char *argv0, char *tokens0, int countExec, return_error)
 {
-	_puts2(argv0); _puts2(": "); print_number(countExec); _puts2(": "); _puts2(tokens0); puts(": not found");
+	_puts2(argv0); _puts2(": "); print_number(countExec); _puts2(": "); _puts2(tokens0); 
+
+	if (returnFunction == -1)
+		perror();
+	else
+		puts(": not found");
 }
