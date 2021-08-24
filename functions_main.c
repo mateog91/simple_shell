86fcff9caa6f15950fc24aa0c0ad9124ec9cc818
mateog91 <mateog91@gmail.com>
2021-08-24 19:58:21 +0000

    added comments to function main_get_line

--- functions_main.c
+++ functions_main.c
@@ -1,5 +1,21 @@
 #include "shell.h"
-
+/**
+ * main_get_line - resolves what to be done with line and frees accordingly
+ * @tokenDirectory: matrix with tokenDirectory of Paths
+ * @tokens: matrix with tokenized line
+ * @pathPtr: pointer to path
+ * @executablePath: pointer with an executable path
+ * @line: pointer to line from command line
+ * @sign: return value from getline
+ * @flag: value of flag
+ * 
+ * Description:
+ * Handles what to be done after getline is executed
+ * If sign is < 0 means a signal of end of file is found
+ * With this, it frees the functions accordingly
+ *
+ * Returns: Nothing
+ */
 void main_get_line(char **tokenDirectory, char **tokens, char *pathPtr,
 		   char *executablePath, char *line, int sign, int flag)
 {
@@ -18,3 +34,5 @@ void main_get_line(char **tokenDirectory, char **tokens, char *pathPtr,
 		exit(1);
 	}
 }
+
+
