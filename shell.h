c1e26955aebd073c8fc11ec76cf3c37a89abcecf
mateog91 <mateog91@gmail.com>
2021-08-24 20:39:59 +0000

    UPD: added main_parse function prototype to shell.h file header file

--- shell.h
+++ shell.h
@@ -38,6 +38,8 @@ typedef struct functions
 /* main functions */
 void main_get_line(char **tokenDirectory, char **tokens, char *pathPtr,
                    char *executablePath, char *line, int sign, int flag);
+int main_parse(char **tokens, char *line, const char *delim);
+
 /* function others */
 int (*check_built_in(char *str_to_check))(char *);
 
